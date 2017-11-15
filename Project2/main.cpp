84579ef82529149a137a78ff8c30cc29fcacde24
ITSIK <ITSIK@LAPTOP-4DKRH4IR>
2017-11-16 01:24:07 +0200

    //we need to check errors like letters of -2 ADDED CONVERT TO INT

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -10,12 +10,24 @@ Element* ParseInput(string filename) {
 	string line;
 	getline(fin, line);
 	size_t num = stoi(line);
+
 	Element* elements = new Element[num]; //remember to delete
+	string id;
+	string left;
+	string top;
+	string right;
+	string bottom;
 	for (int i = 0; i < num; i++) {
-		fin >> elements[i].id;
-		fin >> elements[i].left;
-		fin >> elements[i].top;
-		fin >> elements[i].right;
+		fin >> id;
+		fin >> left;
+		fin >> top;
+		fin >> right;
+		fin >> bottom;
+		//we need to check errors like letters of -2
+		elements[i].id = stoi(id);
+		elements[i].left = stoi(left);
+		elements[i].top = stoi(top);
+		elements[i].right = stoi(right);
 		fin >> elements[i].bottom;
 	}
 	return elements;
@@ -24,7 +36,7 @@ Element* ParseInput(string filename) {
 int main(int argc, char *argv[]) {
 	Element* elements = ParseInput(argv[1]);
 
-	delete [] elements;
+	delete[] elements;
 	int x;
 	cin >> x;
 }
